efb290dc724706cfd14d3edb1320e0e1780b9df9
Rafael Alejandro Renteria Gomez <rarg93@hotmail..com>
2018-10-18 09:56:07 -0500

    mi primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-tareas',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-tareas1';
+  tareas : Tarea[];//arreglo de tareas
+	ultimoId: number;
+
+	constructor(){
+    this.tareas = [{id: 1,titulo: 'Terminar el API',completada: false},
+    {id: 2, titulo: 'Revisar historias de usuario',completada: true},
+    new Tarea({id: 3,titulo: 'Comprar boletos para el cine',completada: false})];
+    this.ultimoId = 3;
+  }
+  
+  agregarTarea(tituloTarea: string){
+    const tareaNueva = new Tarea({titulo: tituloTarea});
+    tareaNueva.id =++this.ultimoId;
+    this.tareas.push(tareaNueva);
+  }
+}
+
+
+class Tarea{
+  id: number;
+  titulo: string;
+  completada : boolean;
+  
+  constructor(valores:object={}){
+  
+      Object.assign(this,valores)
+  }
 }
