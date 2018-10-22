83e9aefc0967ac13dd68d940f94a9434f31cc629
Rafael Alejandro Renteria Gomez <rarg93@hotmail..com>
2018-10-22 09:59:27 -0500

    estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   tareas : Tarea[];//arreglo de tareas
-	ultimoId: number;
+  ultimoId: number;
+  
 
 	constructor(){
     this.tareas = [{id: 1,titulo: 'Terminar el API',completada: false},
     {id: 2, titulo: 'Revisar historias de usuario',completada: true},
     new Tarea({id: 3,titulo: 'Comprar boletos para el cine',completada: false})];
     this.ultimoId = 3;
+  
   }
   
   agregarTarea(tituloTarea: string){
@@ -21,7 +23,29 @@ export class AppComponent {
     tareaNueva.id =++this.ultimoId;
     this.tareas.push(tareaNueva);
   }
-}
+  eliminarTarea(idTarea:number){
+     this.tareas = this.tareas.filter(t => t.id !== idTarea);
+    //  this.tareas = this.tareas.filter(function(tarea){
+    //    return tarea.id!=idTarea;
+    //  })
+
+  }
+  toggleTarea(idTarea: number) {
+    // for (const tarea of this.tareas)  {
+    //   if (tarea.id === idTarea) {
+    //      tarea.completada = !tarea.completada;
+    //      break;
+    //   }
+    // }
+   this.tareas.filter(t => t.id === idTarea?t.completada=!t.completada:null)
+  } 
+
+  tareasCompletadas(): number {
+    return this.tareas.filter(function(tarea) {
+      return tarea.completada === true;
+    }).length;
+  }
+}//fin clase
 
 
 class Tarea{
